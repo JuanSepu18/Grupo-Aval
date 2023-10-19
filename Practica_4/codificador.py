fe50c86dff4afbb8b998f2334b4af173d1644d09
pimientoyolo125 <mgefrad@gmail.com>
2023-10-19 04:27:05 -0500

    Agregango la practica_4

---
+++ Practica_4/codificador.py
@@ -0,0 +1,98 @@
+import random
+
+def codigo_destino(destino):
+    diccionario = {
+        None : "000",
+        "M"  : "001",
+        "D"  : "010",
+        "MD" : "011",
+        "A"  : "100",
+        "AM" : "101",
+        "AD" : "110",
+        "ADM": "111"
+    }
+
+    return diccionario[destino]
+
+def codigo_operacion(operacion):
+
+    operacion = ''.join(operacion.split())
+
+    diccionario = {
+        None : None,
+        "0"  :  "0101010",
+        "1"  :  "0111111",
+        "-1" :  "0111010",
+        "D"  :  "0001100",
+        "A"  :  "0110000",
+        "M"  :  "1110000",
+        "!D" :  "0001101",
+        "!A" :  "0110001",
+        "!M" :  "1110001",
+        "-D" :  "0001111",
+        "-A" :  "0110011",
+        "-M" :  "1110011",
+        "D+1":  "0011111",
+        "A+1":  "0110111",
+        "M+1":  "1110111",
+        "D-1":  "0001110",
+        "A-1":  "0110010",
+        "M-1":  "1110010",
+        "D+A":  "0000010",
+        "D+M":  "1000010",
+        "D-A":  "0010011",
+        "D-M":  "1010011",
+        "A-D":  "0000111",
+        "M-D":  "1000111",
+        "D&A":  "0000000",
+        "D&M":  "1000000",
+        "D|A":  "0010101",
+        "D|M":  "1010101"
+    }
+
+    return diccionario[operacion]
+
+def codigo_salto(salto):
+    diccionario = {
+        None  : "000",
+        "JGT" : "001",
+        "JEQ" : "010",
+        "JGE" : "011",
+        "JLT" : "100",
+        "JNE" : "101",
+        "JLE" : "110",
+        "JMP" : "111"
+    }
+
+    return diccionario[salto]
+
+def codigo_valor(valor, diccionario):
+    binario = ""
+    if valor.isdigit():
+        binario =  bin(int(valor))[2:]
+    elif valor in diccionario:
+        binario =  bin(diccionario[valor])[2:]
+    else:
+        numero = random.randint(16, 16383)
+        while (numero in diccionario.values()):
+            numero = random.randint(16, 16383)
+
+        diccionario[valor] = numero
+
+        binario = bin(numero)[2:]
+
+    while(len(binario) != 15):
+        binario = "0"+ binario
+    
+    return binario, diccionario
+
+def agregar_al_diccionario(diccionario, lineas):
+    contador = 0;
+    for linea in lineas:
+        if(linea.tipo != 'L'):
+            contador = contador +1
+        else:
+            etiqueta = linea.get_etiqueta()
+            diccionario[etiqueta] = contador
+    
+    return diccionario
---
+++ Practica_4/ensamblador.py
@@ -0,0 +1,46 @@
+import sys
+import parseador as ps
+import codificador as cd
+
+diccionario = {
+    "SCREEN" : 16384,
+    "KBD" : 24576,
+    "SP" : 0,
+    "LCL" : 1,
+    "ARG" : 2,
+    "THIS" : 3,
+    "THAT": 4
+}
+
+for i in range(16):
+    llave = "R" + str(i)
+    diccionario[llave] = i
+
+
+def main(diccionario):
+    lineas = []
+    nombre_archivo = sys.argv[1]
+    with open(nombre_archivo, 'r') as archivo:
+        for linea in archivo:
+            linea = ps.parseo(linea)
+            if(linea.tipo == None):
+                continue
+            lineas.append(linea)
+    
+    diccionario = cd.agregar_al_diccionario(diccionario, lineas)
+
+    nombre_archivo_hack = nombre_archivo[:nombre_archivo.find('.')] + ".hack"
+    with open(nombre_archivo_hack, 'w') as archivo_hack:
+        for i in lineas:
+            instruccion = ""
+            if(i.tipo == 'C'):
+                instruccion = "111" + cd.codigo_operacion(i.get_operacion()) + cd.codigo_destino(i.get_destino()) + cd.codigo_salto(i.get_salto())
+                archivo_hack.write(instruccion + '\n')
+            elif(i.tipo == 'A'):
+                instruccion, diccionario = cd.codigo_valor(i.get_valor(), diccionario)
+                instruccion = "0" +  instruccion
+                archivo_hack.write(instruccion + '\n')
+            
+
+if __name__ == '__main__':
+    main(diccionario)
